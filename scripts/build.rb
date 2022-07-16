b83b1e0536474f7a66ad9e39f2414e6ce81819a2
Viktor Kuzmin <kvaster@gmail.com>
2022-07-16 11:46:24 +0300

    Remove old hacks

--- scripts/build.rb
+++ scripts/build.rb
@@ -443,7 +443,7 @@ class Builder
       puts 'Updating system'
       chrun [
         'emerge -qe @system --keep-going --with-bdeps=y',
-        'emerge -q1u sys-fs/udev sys-apps/systemd-tmpfiles', # remove after migration
+        'emerge -qDuN @world --keep-going --with-bdeps=y'
         'emerge -q --depclean',
         'etc-update --automode -5',
         'eselect news read',
